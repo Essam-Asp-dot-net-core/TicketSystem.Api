b740dc0978b37aebf657950d289180d95d1b9a18
Essam-Asp-dot-net-core <ahmedessam1144911@gmail.com>
2025-05-23 01:32:21 +0300

    Update2

---
+++ TicketSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Ticket.Core.Entity;
+using Ticket.Repository.Data.AppDbContext;
+using TicketSystem.Api.DTOs;
+
+namespace TicketSystem.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly ApplicationDbContext _dbContext;
+		private readonly UserManager<User> _userManager;
+		private readonly SignInManager<User> _signInManager;
+		private readonly IConfiguration _configuration;
+
+		public AuthController(ApplicationDbContext dbContext , UserManager<User> userManager , SignInManager<User> signInManager , IConfiguration configuration)
+		{
+			_dbContext = dbContext;
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_configuration = configuration;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult>Register(RegisterDTO model)
+		{
+			var exist = await _userManager.FindByNameAsync(model.Username);
+			if (exist != null)
+				return BadRequest("Your Name Is Exist");
+
+			var user = new User
+			{
+				FullName = model.Username,
+				Role = model.Role,
+			};
+			var result = await _userManager.CreateAsync(user , model.Password);
+			if (!result.Succeeded)
+				return BadRequest(result.Errors);
+			return Ok("Register Successfully");
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult>Login(LoginDTO dto)
+		{
+			var user =await _userManager.FindByNameAsync(dto.Username);
+			if(user == null)
+				return Unauthorized("This Name Is InValid");
+			var UserLogin = await _signInManager.CheckPasswordSignInAsync(user, dto.Password, false);
+			if (!UserLogin.Succeeded)
+				return Unauthorized("Password is InCorrect");
+			var Claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.GivenName, user.FullName),
+				new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+				new Claim(ClaimTypes.Role, user.Role)
+
+			};
+			var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+			var credential = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256Signature);
+
+			var token = new JwtSecurityToken(
+
+				issuer: _configuration["Jwt:Issure"],
+				audience: _configuration["Jwt:Audience"],
+				claims:Claims,
+				expires: DateTime.Now.AddDays(double.Parse(_configuration["Jwt:Expire"])),
+				signingCredentials:credential
+				);
+
+			return Ok(new {token = new JwtSecurityTokenHandler().WriteToken(token)});
+
+
+		}
+	}
+}
--- TicketSystem.Api/Program.cs
+++ TicketSystem.Api/Program.cs
@@ -1,45 +1,65 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Ticket.Repository.Data.AppDbContext;
 
 namespace TicketSystem.Api
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+			// Add services to the container.
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
 
-            //DbContext For Application
-            builder.Services.AddDbContext<ApplicationDbContext>(Option =>
-            {
-                Option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
+			//DbContext For Application
+			builder.Services.AddDbContext<ApplicationDbContext>(Option =>
+			{
+				Option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
 
-            //Services
-            builder.Services.AddAutoMapper(typeof(Program));
-            var app = builder.Build();
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+							.AddJwtBearer(options =>
+		{
+			options.TokenValidationParameters = new TokenValidationParameters
+			{
+				ValidateIssuer = true,
+				ValidateAudience = true,
+				ValidateLifetime = true,
+				ValidateIssuerSigningKey = true,
+				ValidIssuer = builder.Configuration["Jwt:Issure"],
+				ValidAudience = builder.Configuration["Jwt:Audience"],
+				IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+			};
+		});
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+			builder.Services.AddAuthorization();
 
 
-            app.MapControllers();
+			//Services
+			builder.Services.AddAutoMapper(typeof(Program));
+			var app = builder.Build();
 
-            app.Run();
-        }
-    }
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
 }
